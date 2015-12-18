f5768cae8119f0cab8a6f4bb89c5432f51f5fea6
chrismeyers <cm.02.93@gmail.com>
2015-12-18 17:44:10 -0500

    added basic login form.

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rebel Scrum Homepage</title>
+        <link rel="shortcut icon" href="images/icons/icon.ico">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="Author" content="rebel-scrum" />
+
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+
+        <link rel="stylesheet" href="css/csselements_mobile.css" />
+
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+    </head>
+
+    <body>
+        <h1>Welcome!</h1>
+        <table>
+            <tr>
+                <td>email:</td>
+                <td><input class="inputbox-mod" type="text" placeholder="you@email.com" name="user"></td>
+            </tr>
+            <tr>
+                <td>password:</td>
+                <td><input class="inputbox-mod" type="text" placeholder="Password" name="password"></td>
+            </tr>
+            <tr>
+                <td><button class="submit-button" type="submit">Login</button></td>
+            </tr>
+        </table>
+        <br /> <br />
+        <table>
+            <tr>
+                <td>New User?</td>
+                <td><button class="register-button" type="submit">Register</button></td>
+            </tr>
+        </table>
+            
+    </body>
+</html>
