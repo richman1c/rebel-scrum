d5c585e7cf45df185052cf3b1ca74e8bfae72114
chrismeyers <cm.02.93@gmail.com>
2015-12-19 06:45:23 -0500

    map takes input from clicks.

--- plan.php
+++ plan.php
@@ -12,75 +12,59 @@
       }
 
     </style>
-            <script src="//maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"></script>
+            
+    <link rel="stylesheet" href="../css/csselements_mobile.css" />
 
   </head>
   <body>
-    <div id="map" style="width:300px;height:300px;"></div>
-  
-    <script>
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 11,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      
-      var place1 = new google.maps.LatLng(39.958120, -74.980072);
-      var place2 = new google.maps.LatLng(39.971898, -75.000591);
-      var place3 = new google.maps.LatLng(39.991694, -75.004106);
-      
-      var userTrip = [pos, place1, place2, place3];
-      var userPath = new google.maps.Polygon({
-          path:userTrip,
-          strokeColor:"#FF0000 ",
-          strokeOpacity:0.5,
-          strokeWidth:2,
-          fillColor:"#FF0000",
-          fillOpacity:0.5
-      });
-
-      userPath.setMap(map);
-      map.setCenter(pos);
-
-     // infoWindow.setPosition(pos);
-     // infoWindow.setContent('Location found.');
-
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+    <div id="dvMap" style="width:300px;height:300px;"></div>
+  <script type="text/javascript">
+    var points = [];
+    
+    window.onload = function () {
+    var mapOptions = {
+        center: new google.maps.LatLng(39.958120, -74.980072),
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("dvMap"), mapOptions);
+ 
+    //Attach click event handler to the map.
+    google.maps.event.addListener(map, 'click', function (e) {
+ 
+        //Determine the location where the user has clicked.
+        var location = e.latLng;
+ 
+        //Create a marker and placed it on the map.
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+ 
+        //Attach click event handler to the marker.
+        google.maps.event.addListener(marker, "click", function (e) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: 'Latitude: ' + location.lat() + '<br />Longitude: ' + location.lng()
+            });
+            infoWindow.open(map, marker);
+            
+        });
+        //add to point array
+        points.push(location.lat());
+        console.log("lat: " + location.lat());
+        points.push(location.lng());
+        console.log("lng: " + location.lng());
     });
+};
+</script>
 
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
-
-    </script>
+    
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyASHj_0c7RUS7SYwxTBmKs5MpSF9ZO6MGw&signed_in=true&callback=initMap"
         async defer>
     </script>
-
+    <br />
+      <form action="../plan.php" method="post" target="_top">
+            <button class="submit-button-long" type="submit">Plan Trip</button>
+      </form>
   </body>
 </html>
