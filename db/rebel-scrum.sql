f798ac922e3c09ab65a9336fcb74ddabba7d55e3
richman1c <rich.r.collins@gmail.com>
2015-12-19 06:50:00 -0500

    added client server state functions
    how does this even work?!

--- db/rebel-scrum.sql
+++ db/rebel-scrum.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2015 at 09:43 AM
+-- Generation Time: Dec 19, 2015 at 12:46 PM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -97,6 +97,13 @@ CREATE TABLE `auth` (
   `pass` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `auth`
+--
+
+INSERT INTO `auth` (`p_key`, `person_key`, `userID`, `pass`) VALUES
+(18, 40, 'test@guy.com', 'test');
+
 -- --------------------------------------------------------
 
 --
@@ -113,6 +120,16 @@ CREATE TABLE `locations` (
   `trip_name` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='table of location pins for mapping';
 
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`lat`, `lon`, `time_stamp`, `person_key`, `loc_type`, `trip_name`) VALUES
+(39.9, -75.01, '2015-12-19 11:30:37', 40, 'TRIP', 'TESTTRIP1'),
+(39.9, -75, '2015-12-19 11:31:53', 40, 'TRIP', 'TESTTRIP1'),
+(39.9, -75.02, '2015-12-19 11:32:48', 40, 'TRIP', 'TESTTRIP1'),
+(39.89, -75.015, '2015-12-19 11:37:38', 40, 'TRIP', 'TESTTRIP1');
+
 -- --------------------------------------------------------
 
 --
@@ -126,6 +143,14 @@ CREATE TABLE `occupant` (
   `emerContact` int(11) NOT NULL,
   `primaryFlag` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `occupant`
+--
+
+INSERT INTO `occupant` (`p_key`, `person_key`, `emerContact`, `primaryFlag`) VALUES
+(10, 40, 41, 1),
+(11, 40, 42, 1);
 
 -- --------------------------------------------------------
 
@@ -143,6 +168,15 @@ CREATE TABLE `person` (
   `email` varchar(50) NOT NULL,
   `dob` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `person`
+--
+
+INSERT INTO `person` (`p_key`, `lname`, `fname`, `phone1`, `phone2`, `email`, `dob`) VALUES
+(40, 'guy', 'test', '1111111111', NULL, 'test@guy.com', '1982-03-06'),
+(41, 'contact1', 'emer', '2222222222', NULL, 'emer@contact1.com', NULL),
+(42, 'contact2', 'emer', '3333333333', NULL, 'emer@contact2.com', NULL);
 
 --
 -- Indexes for dumped tables
@@ -176,17 +210,17 @@ ALTER TABLE `person`
 -- AUTO_INCREMENT for table `auth`
 --
 ALTER TABLE `auth`
-  MODIFY `p_key` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `p_key` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `occupant`
 --
 ALTER TABLE `occupant`
-  MODIFY `p_key` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `p_key` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `person`
 --
 ALTER TABLE `person`
-  MODIFY `p_key` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `p_key` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- lib/server-client.php
+++ lib/server-client.php
@@ -1,13 +1,37 @@
 <?php
-    session_start();
-    
+    include('lib/dbconnect.php'); 
     function serverDataToCookie($someData)
     {
-    
+        $array = array();
+        
+        while($row = mysqli_fetch_array($someData,MYSQL_ASSOC))
+        {
+            array_push($array,$row);
+        }
+        $jsonData = json_encode($array);
+        echo $jsonData;
+        //TODO: APPEND IF COOKIE HAS DATA
+        setcookie("clientlistofpoints",$jsonData);
     };
     
     function serverDataFromCookie()
     {
+        if(isset($_COOKIE["serverlistofpoints"]))
+        {
+        $jsonData = $_COOKIE["serverlistofpoints"];
+        $array = json_decode($jsonData);
+        foreach($row in $array)
+        {
+            $sql = "CALL updateLocation(?,?,?,?,?)";
+            $stmt = mysqli_prepare($con,$sql);
+            mysqli_stmt_bind_param($stmt,'ddsss',$row['lat'],$row['lon'],$_SESSION['userID'],$row['loc_type'],$row['trip_name']);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_cluse($stmt);
+        }
+        unset($_COOKIE["serverlistofpoints"]);
+        setcookie("serverlistofpoints", "", time()-3600);
+        mysqli_close($con);
+        }
     };
     
  ?>
@@ -17,10 +41,34 @@
     
     function clientDataToCookie(someData)
     {
+        //parse someData into json format
+        var json = JSON.stringify(someData);
+        //set/append cookie 'serverlistofpoints' to jsondata
+        document.cookie = 'serverlistofpoints='+json+';';
+    }
+    
+    function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
     }
     
     function clientDataFromCookie()
     {
+        //get value of cookie 'clientlistofpoints'
+        //populate local data structure with decoded json data
+        
+        var array = jQuery.parseJSON(unescape(getCookie("clientlistofpoints")));        
+        //delete cookie 'clientlistofpoints'
+        document.cookie = 'clientlistofpoints=; Max-Age=0'
+        //return local data structure for further processing
+        return array;
+        
     }
  </script>
  
